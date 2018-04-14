fef6ce73613589ef90883589705d886e5eb4fd9c
Mohan Pichikala <mohan.pitchikala@sumtotalsystems.com>
2018-04-14 15:01:56 +0530

    added the code for multiple dependency and for sorting

--- parser.rb
+++ parser.rb
@@ -1,17 +1,55 @@
 require './process_job'
 require 'pry'
+require 'set'
 
 class Parser
-  def parse_params(params)
+  #parses the initial params
+  def self.parse_params(params)
     params.split("\n").inject({}){|result,element|
       process_job = ProcessJob.new(*element.split('=>').map(&:strip))
       result[process_job.right_node] = process_job
       result
     }
-    puts params
+  end
+
+  #Needs to sort data to check the multiple job dependency
+  def parse_input(params)
+    puts Parser.sort_jobs!(Parser.parse_params(params))
+  end
+
+  def self.check_cyclic_dependancy(job_params)
+    return nil if job_params.nil?
+
+    job_params.each do |ele, job|
+      look_up = Set.new
+      while(job = job_params[job.left_node])
+        break if !job.left_node
+      end
+    end
+    return false
+  end
+
+  def self.sort_jobs!(data)
+    raise DenpendencyError.new("Job doesn't depend on the same") if self.check_cyclic_dependancy(data)
+
+    data.values.inject(Array.new) { |elements, obj|
+      if !elements.include? obj.right_node
+        elements << obj.right_node
+      end
+      if obj.left_node
+        elements.delete(obj.left_node)
+        job_pos = elements.index(obj.right_node)
+        elements.insert(job_pos, obj.left_node)
+      end
+      elements
+    }
   end
 end
-Parser.new.parse_params("a =>
-b => c
-c =>")
-Parser.new.parse_params("a => a")
+
+Parser.new.parse_input("a =>
+                         b => c
+                         c => f
+                         d => a
+                         e => b
+                         f => ")
+# Parser.new.parse_input("a => a")
